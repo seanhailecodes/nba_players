51bea56da1576d36fc4d3be2b7f86ee6e71874a2
Your Name <you@example.com>
2019-05-08 12:37:39 -0400

    add auth to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import decode from 'jwt-decode';
+
+import Login from './components/Login'
+import Register from './components/Register'
+
+import {
+  loginUser,
+  registerUser
+} from './services/api-helper'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: null,
+      authFormData: {
+        username: "",
+        email: "",
+        password: ""
+      }
+    }
+
+    this.handleLoginButton = this.handleLoginButton.bind(this)
+    this.handleLogin = this.handleLogin.bind(this)
+    this.handleRegister = this.handleRegister.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+    this.authHandleChange = this.authHandleChange.bind(this)
+  }
+
+  handleLoginButton() {
+    this.props.history.push("/login")
+  }
+
+  async handleLogin() {
+    const userData = await loginUser(this.state.authFormData);
+    this.setState({
+      currentUser: decode(userData.token)
+    })
+    localStorage.setItem("jwt", userData.token)
+  }
+
+  async handleRegister(e) {
+    e.preventDefault();
+    await registerUser(this.state.authFormData);
+    this.handleLogin();
+  }
+
+  handleLogout() {
+    localStorage.removeItem("jwt");
+    this.setState({
+      currentUser: null
+    })
+  }
+
+  authHandleChange(e) {
+    const { name, value } = e.target;
+    this.setState(prevState => ({
+      authFormData: {
+        ...prevState.authFormData,
+        [name]: value
+      }
+    }));
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>NBA Players App</h1>
+          <div>
+            {this.state.currentUser
+              ?
+              <>
+                <p>{this.state.currentUser.username}</p>
+                <button onClick={this.handleLogout}>logout</button>
+              </>
+              :
+              <button onClick={this.handleLoginButton}>Login/register</button>
+            }
+          </div>
+        </header>
+        <Route exact path="/login" render={() => (
+          <Login
+            handleLogin={this.handleLogin}
+            handleChange={this.authHandleChange}
+            formData={this.state.authFormData} />)} />
+        <Route exact path="/register" render={() => (
+          <Register
+            handleRegister={this.handleRegister}
+            handleChange={this.authHandleChange}
+            formData={this.state.authFormData} />)} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
